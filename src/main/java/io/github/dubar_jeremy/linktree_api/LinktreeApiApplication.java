914bf3c3578fec06dea65dbb26b73903902bec41
Dubar Jérémy <jeremy.dubar@decathlon.com>
2025-12-30 11:10:48 +0100

    init

---
+++ src/main/java/io/github/dubar_jeremy/linktree_api/LinktreeApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.dubar_jeremy.linktree_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinktreeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinktreeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dubar_jeremy/linktree_api/LinktreeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dubar_jeremy.linktree_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinktreeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

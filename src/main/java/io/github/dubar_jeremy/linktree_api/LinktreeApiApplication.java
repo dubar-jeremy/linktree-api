c77797f4531520eaf69bd6c8c0482410e3f33949
dubar-jeremy <dubar.jeremy@gmail.com>
2026-01-17 13:26:02 +0100

    feat: page link entities (#3)

--- src/main/java/io/github/dubar_jeremy/linktree_api/LinktreeApiApplication.java
+++ src/main/java/io/github/dubar_jeremy/linktree_api/LinktreeApiApplication.java
@@ -2,6 +2,7 @@ package io.github.dubar_jeremy.linktree_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class LinktreeApiApplication {

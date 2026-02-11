228a73edf384c4ccd191439b30c1ab53e15b3b5a
Dubar Jérémy <jeremy.dubar@decathlon.com>
2026-02-11 10:53:03 +0100

    feat(page): Add checked exception + usage in PageService

--- src/main/java/io/github/dubar_jeremy/linktree_api/service/PageService.java
+++ src/main/java/io/github/dubar_jeremy/linktree_api/service/PageService.java
@@ -1,5 +1,6 @@
 package io.github.dubar_jeremy.linktree_api.service;
 
+import io.github.dubar_jeremy.linktree_api.exception.SlugAlreadyExistsException;
 import io.github.dubar_jeremy.linktree_api.model.Page;
 import io.github.dubar_jeremy.linktree_api.provider.SlugProvider;
 import io.github.dubar_jeremy.linktree_api.provider.UuidProvider;
@@ -19,12 +20,31 @@ public class PageService {
         this.slugProvider = slugProvider;
     }
 
-    public Page createPage(String title, String description) {
+    /**
+     * The method throws the exception and ask the method that calls this one to catch it and handle it (Controller).
+     * Check in review session if this is the best way to handle this or if I should catch the exception here and return an error response.
+     */
+    public Page createPage(String title, String description) throws SlugAlreadyExistsException {
         Page page = new Page();
         page.setId(uuidProvider.generate());
         page.setTitle(title);
-        page.setSlug(slugProvider.generate(title));
+        page.setSlug(this.generateSlug(title));
         page.setDescription(description);
         return pageRepository.save(page);
+    }
+
+    /*
+     * Here I throw the exception and ask the method that calls this one to catch it and handle it.
+     */
+    private String generateSlug(String title) throws SlugAlreadyExistsException {
+
+        // that one throw a runtime exception. I let the framework handle it.
+        String slug = slugProvider.generate(title);
+
+        if (pageRepository.existsBySlug(slug)) {
+            throw new SlugAlreadyExistsException(slug);
+        }
+
+        return slug;
     }
 }

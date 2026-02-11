05ccf03cefa43b916dde9b9f52775988015d43fc
Dubar Jérémy <jeremy.dubar@decathlon.com>
2026-02-11 10:31:46 +0100

    feat: add SlugException for handling slug-related errors

---
+++ src/main/java/io/github/dubar_jeremy/linktree_api/exception/SlugException.java
@@ -0,0 +1,8 @@
+package io.github.dubar_jeremy.linktree_api.exception;
+
+public class SlugException extends RuntimeException {
+
+    public SlugException(String message) {
+        super(message);
+    }
+}

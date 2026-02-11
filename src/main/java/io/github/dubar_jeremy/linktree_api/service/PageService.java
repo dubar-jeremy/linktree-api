deafc29191606fcaf8da978f379ae741a1d0cf0a
Dubar Jérémy <jeremy.dubar@decathlon.com>
2026-02-11 10:33:57 +0100

    feat(page): integrate SlugProvider for generating slugs from titles

--- src/main/java/io/github/dubar_jeremy/linktree_api/service/PageService.java
+++ src/main/java/io/github/dubar_jeremy/linktree_api/service/PageService.java
@@ -1,6 +1,7 @@
 package io.github.dubar_jeremy.linktree_api.service;
 
 import io.github.dubar_jeremy.linktree_api.model.Page;
+import io.github.dubar_jeremy.linktree_api.provider.SlugProvider;
 import io.github.dubar_jeremy.linktree_api.provider.UuidProvider;
 import io.github.dubar_jeremy.linktree_api.repository.PageRepository;
 import org.springframework.stereotype.Service;
@@ -10,19 +11,20 @@ public class PageService {
 
     private final PageRepository pageRepository;
     private final UuidProvider uuidProvider;
+    private final SlugProvider slugProvider;
 
-    public PageService(PageRepository pageRepository, UuidProvider uuidProvider) {
+    public PageService(PageRepository pageRepository, UuidProvider uuidProvider, SlugProvider slugProvider) {
         this.pageRepository = pageRepository;
         this.uuidProvider = uuidProvider;
+        this.slugProvider = slugProvider;
     }
-
 
     public Page createPage(String title, String description) {
         Page page = new Page();
         page.setId(uuidProvider.generate());
         page.setTitle(title);
+        page.setSlug(slugProvider.generate(title));
         page.setDescription(description);
-        page.setSlug(uuidProvider.generate().toString()); //  TO IMPLEMENT
         return pageRepository.save(page);
     }
 }
